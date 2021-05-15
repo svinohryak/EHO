05f9938c378baad0d7bc3cfcf622663d2a698cd2
svinohryak <shcheglovmusician@gmail.com>
2021-05-15 16:25:45 +0400

    Big Img Gallery without LightBox

--- js/script.js
+++ js/script.js
@@ -3,28 +3,51 @@ const nextBtn = document.querySelector('#next');
 let i = 0;
 let images = document.querySelectorAll('.photo-gallery > img');
 
-function prevImg() {
-    images[i].className = '';
-    i--;
-    if(i < 0) {
-        i = images.length -1;
+// function prevImg() {
+//     images[i].className = '';
+//     i--;
+//     if(i < 0) {
+//         i = images.length -1;
+//     }
+//     images[i].className = 'shown';
+// }
+
+// function nextImg() {
+//     images[i].className = '';
+//     i++;
+//     if(i >= images.length) {
+//         i = 0;
+//     } 
+//     images[i].className = 'shown';
+// }
+
+// if(prevBtn) {
+//     prevBtn.addEventListener('click', prevImg);
+// }
+// if(nextBtn) {
+//     nextBtn.addEventListener('click', nextImg);
+// }
+
+if(prevBtn && nextBtn) {
+    function prevImg() {
+        images[i].className = '';
+        i--;
+        if(i < 0) {
+            i = images.length -1;
+        }
+        images[i].className = 'shown';
     }
-    images[i].className = 'shown';
-}
+    
+    function nextImg() {
+        images[i].className = '';
+        i++;
+        if(i >= images.length) {
+            i = 0;
+        } 
+        images[i].className = 'shown';
+    }
 
-function nextImg() {
-    images[i].className = '';
-    i++;
-    if(i >= images.length) {
-        i = 0;
-    } 
-    images[i].className = 'shown';
-}
-
-if(prevBtn) {
     prevBtn.addEventListener('click', prevImg);
-}
-if(nextBtn) {
     nextBtn.addEventListener('click', nextImg);
 }
 
